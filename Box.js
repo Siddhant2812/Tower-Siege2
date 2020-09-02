18ab9eb749a5c760ee91407a8b5efe24c7375867
Siddhant2812 <68544278+Siddhant2812@users.noreply.github.com>
2020-09-02 18:17:57 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,50 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.4,
+          'friction':0.12,
+          'isStatic':false    
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.Visiblity = 255
+      
+      World.add(world, this.body);
+    }
+    display(){
+
+      //console.log(this.body.speed);
+
+       if(this.body.speed<6){
+          var pos =this.body.position;
+          var angle =this.body.angle;
+          push();
+          translate(pos.x,pos.y);
+          rotate(angle); 
+          rectMode(CENTER);
+          fill("pink");
+          rect(0, 0, this.width, this.height);
+          pop();
+     }
+      else{
+        World.remove(world, this.body);
+        push();
+        this.Visiblity = this.Visiblity - 5;
+        pop();
+    }
+      
+
+      /*if(this.body.speed>2){
+        World.remove(world, this.body);
+        push();
+        this.Visiblity = this.Visiblity - 5;
+        tint(255,this.Visiblity);
+        rect(this.body.position.x, this.body.position.y, 50, 50);
+        pop();
+      }*/
+
+      
+    }
+  }
+  
